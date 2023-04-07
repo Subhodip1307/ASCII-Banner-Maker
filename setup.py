351c83627ce8bfec84d2a1287996800bf4f83f1f
Subhodip Sardar <sardarsubhodip857@gmail.com>
2023-04-07 20:16:53 +0530

    setup file

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+import platform
+os_name=(platform.system())
+if os_name=="Windows":
+    os.system("pip install pandas")
+    os.system("pip install colorama")
+    os.system("pip install colorama")
+elif os_name=="Mac":
+    os.system("pip install pandas")
+    os.system("pip install colorama")
+    os.system("pip install colorama")
+else:
+    os.system("pip3 install pandas")
+    os.system("pip3 install colorama")
+    os.system("pip3 install colorama")
