3cec3a2a066ab21803278dfd5846a4f221f8fd37
Subhodip Sardar <sardarsubhodip857@gmail.com>
2023-04-07 21:51:54 +0530

    Updated setup file

--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ os_name=(platform.system())
 if os_name=="Windows":
     os.system("pip install pandas")
     os.system("pip install colorama")
-    os.system("pip install colorama")
+    os.system("pip install pyfiglet")
+
 elif os_name=="Mac":
     os.system("pip install pandas")
     os.system("pip install colorama")
-    os.system("pip install colorama")
+    os.system("pip install pyfiglet")
 else:
     os.system("pip3 install pandas")
     os.system("pip3 install colorama")
-    os.system("pip3 install colorama")
+    os.system("pip3 install pyfiglet")
